8b2e4e33bc4c11196ad1ab0bcb8eb90db30eac56
Balagur Dmitry <farnitin@yandex.ru>
2025-06-04 15:00:16 +0400

    add ItemDto

---
+++ src/main/java/ru/shareit/item/ItemDto.java
@@ -0,0 +1,10 @@
+package ru.shareit.item;
+
+import ru.shareit.user.User;
+
+public class ItemDto {
+    private String name;
+    private String description;
+    private boolean available;
+    private User owner;
+}
