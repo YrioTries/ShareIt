b5788bcec7580f41224279458286c6e4f4dec1b1
Dmitry <farnitin@yandex.ru>
2025-06-05 01:35:04 +0400

    add skillet of ItemMapper

--- src/main/java/ru/shareit/item/ItemMapper.java
+++ src/main/java/ru/shareit/item/ItemMapper.java
@@ -1,4 +1,13 @@
 package ru.shareit.item;
 
 public class ItemMapper {
+    public static ItemDto toItemDto(Item item) {
+        return new ItemDto(
+                item.getName(),
+                item.getDescription(),
+                item.isAvailable(),
+                item.getRequest() != null ? item.getRequest().getId() : null
+        );
+    }
+
 }
